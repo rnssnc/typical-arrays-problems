08f565e6f54172b08d77d1dacb06c8e7093c528e
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-01-20 14:40:46 +0300

    feat: complete all required functions (min,max,avg)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) return 0;
+    array.sort((a, b) => a - b);
+    return array[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) return 0;
+    array.sort((a, b) => b - a);
+    return array[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) return 0;
+    return array.reduce((a, b) => a + b) / array.length;
+};
